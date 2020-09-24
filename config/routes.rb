edcdec5cd2e4cd24e4d59ad76b37271a186415b6
sheep1755 <the_straw_hat@icloud.com>
2020-09-24 15:00:20 +0900

    delete-ds_store1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "worktimes#index"
-  resources :worktimes, only: [:index, :create, :show]
+  resources :worktimes, only: [:index, :create, :show, :edit, :update]
   resources :holidays, only: [:new, :create]
   post '/holidays/new' => 'holidays#new'
+  post '/worktimes/show' => 'worktimes#show'
 end
